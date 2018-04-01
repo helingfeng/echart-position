42217120217af18a6fcd182642dfd8568d5cad4a
helingfeng <857190327@qq.com>
2018-04-01 10:02:05 +0800

    seed

--- app/Console/Commands/TestSpider.php
+++ app/Console/Commands/TestSpider.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\EventDispatcher\GenericEvent;
+use VDB\Spider\Discoverer\DiscovererSet;
 use VDB\Spider\Discoverer\XPathExpressionDiscoverer;
 use VDB\Spider\Event\SpiderEvents;
 use VDB\Spider\Spider;
@@ -50,7 +51,7 @@ class TestSpider extends Command
         $spider->getDiscovererSet()->set(new XPathExpressionDiscoverer("//div[@class='company-place']//a"));
 
         $spider->getDiscovererSet()->maxDepth = 1;
-        $spider->getQueueManager()->maxQueueSize = 10000;
+        $spider->getQueueManager()->maxQueueSize = 100;
 
         $statsHandler = new StatsHandler();
         $spider->getDispatcher()->addSubscriber($statsHandler);
@@ -60,6 +61,20 @@ class TestSpider extends Command
 
         $spider->getDispatcher()->addListener(
             SpiderEvents::SPIDER_CRAWL_RESOURCE_PERSISTED,
+            function (GenericEvent $event) use ($output) {
+                $output->writeln($event->getArguments());
+            }
+        );
+
+        $spider->getDispatcher()->addListener(
+            SpiderEvents::SPIDER_CRAWL_PRE_REQUEST,
+            function (GenericEvent $event) use ($output) {
+                $output->writeln($event->getArguments());
+            }
+        );
+
+        $spider->getDispatcher()->addListener(
+            SpiderEvents::SPIDER_CRAWL_ERROR_REQUEST,
             function (GenericEvent $event) use ($output) {
                 $output->writeln($event->getArguments());
             }
@@ -76,13 +91,13 @@ class TestSpider extends Command
         $output->writeln("获取资源: ");
         foreach ($spider->getDownloader()->getPersistenceHandler() as $resource) {
             $output->writeln(" - " . $resource->getCrawler()->filterXpath('//title')->text());
-            DB::table('pt_origin_html')->insert([
-                'title' => $resource->getCrawler()->filterXpath('//title')->text(),
-                'url' => $resource->getUri(),
-                'html' => $resource->getResponse()->getBody(),
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s'),
-            ]);
+//            DB::table('pt_origin_html')->insert([
+//                'title' => $resource->getCrawler()->filterXpath('//title')->text(),
+//                'url' => $resource->getUri(),
+//                'html' => $resource->getResponse()->getBody(),
+//                'created_at' => date('Y-m-d H:i:s'),
+//                'updated_at' => date('Y-m-d H:i:s'),
+//            ]);
         }
     }
 }
